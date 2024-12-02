1df774d8e1d9346d8d0bb56c6ff8a902a14ca7c8
neuwa <y.liverdoold@gmail.com>
2024-12-01 22:03:37 -0500

    Added badge for Github Actions

--- service/routes.py
+++ service/routes.py
@@ -58,7 +58,6 @@ def create_accounts():
     )
 
 
-
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -76,6 +75,7 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -92,6 +92,7 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -112,6 +113,7 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
